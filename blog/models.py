2bcc721410a3e7659d982393929be9be56f54021
LeoKnox <LeoKnox@users.noreply.github.com>
2022-07-04 18:34:59 -0700

    Add files via upload

--- blog/models.py
+++ blog/models.py
@@ -20,5 +20,11 @@ class Post(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     class Meta:
         ordering = ('-publish',)
+    def get_absolute_url(self):
+        return reversse('blog:post_detail',
+                            args=[self.publish.year,
+                                    self.publish.month,
+                                    self.publish.day,
+                                    self.slug])
     def __str__(self):
         return self.title

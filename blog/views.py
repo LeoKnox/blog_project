889ec8e8c0eb021e4f2b673858a82ac6e2ccea8a
LeoKnox <LeoKnox@users.noreply.github.com>
2022-07-05 22:11:47 -0700

    Add files via upload

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm
@@ -6,13 +7,21 @@ from .models import Post
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read "\
+                        f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                        f"{cd['name']}\'s comments: {cd['comment']}"
+            send_mail(subject, message, 'admin@blog.com', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
 
 def post_list(request):
     object_list = Post.published.all()

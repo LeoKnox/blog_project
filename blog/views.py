a36cc998824e69aef8b8814b325053ec8e002c42
LeoKnox <LeoKnox@users.noreply.github.com>
2022-07-05 21:35:43 -0700

    Add files via upload

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
 from .models import Post
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
 
 def post_list(request):
     object_list = Post.published.all()

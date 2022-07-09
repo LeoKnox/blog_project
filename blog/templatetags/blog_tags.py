6ebf477b34674f20cde6f402be2e03ebe6fe81b7
LeoKnox <LeoKnox@users.noreply.github.com>
2022-07-08 22:18:06 -0700

    Add files via upload

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -10,4 +10,4 @@ def total_posts():
 @register.inclusion_tag('blog/post/latest_posts.html')
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
-    return {'latest_posts': lastest_posts}
+    return {'latest_posts': latest_posts}

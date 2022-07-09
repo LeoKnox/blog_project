a06cb34916d66ac71757ef058dbfb27e3815c9f8
LeoKnox <LeoKnox@users.noreply.github.com>
2022-07-08 22:32:27 -0700

    Add files via upload

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,7 +1,12 @@
 from django import template
+from django.db.models import Count
 from ..models import Post
 
 register = template.Library()
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
 
 @register.simple_tag
 def total_posts():

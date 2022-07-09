7b2ea85e8834fec677df1bbfc7ee12e747d2a378
LeoKnox <LeoKnox@users.noreply.github.com>
2022-07-08 22:38:51 -0700

    Add files via upload

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,8 +1,14 @@
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
 from ..models import Post
+import markdown
 
 register = template.Library()
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
 
 @register.simple_tag
 def get_most_commented_posts(count=5):
